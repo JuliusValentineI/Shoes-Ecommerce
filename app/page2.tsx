5697be65363ce8a76a607a10770a3c9b2953a784
JuliusValentineI <hernanigonzalez.personal@gmail.com>
2024-01-15 23:19:37 -0600

    _app and index push

--- app/page2.tsx
+++ app/page2.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h1> Control + k </h1>
+    <h1> Welcome home </h1>
   )
 }
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return(
+    <div>
+      <main>   
+	<h1>
+	  Welcome to <span> NextJs</span>, TailwindCss and Typescript
+	</h1>
+      </main>
+    </div>
+  )
+}
