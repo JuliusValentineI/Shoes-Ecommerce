e553967edc0c8699ba5d033bcc0ab10ef9648bca
JuliusValentineI <hernanigonzalez.personal@gmail.com>
2024-01-16 18:06:21 -0600

    Navbar structure create in components/Navbar/Navbar.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import Navbar from './components/Nabvar/Navbar'
+
 export default function Home() {
   return(
     <div>
+    <Navbar/>
       <main>   
 	<h1>
 	  Welcome to <span> NextJs</span>, TailwindCss and Typescript
